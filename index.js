c6229c6ac6537df3eedc384c3453de3deee89498
Matt Currier <fingerskier@gmail.com>
2017-01-04 08:04:22 -0600

    Update

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var fs = require('fs')
-    , util = require('util')
-    , stream = require('stream')
-    , es = require('event-stream');
+var util = require('util')
+var stream = require('stream')
+var es = require('event-stream');
 
 
 module.exports = function(filepath){
-    fs.createReadStream(filepath)
-        .pipe(es.split())
-        .pipe(es.mapSync(function(line){
+	fs.createReadStream(filepath)
+		.pipe(es.split())
+		.pipe(es.mapSync(function(line){
 
-            // pause the readstream
-            s.pause();
+			// pause the readstream
+			s.pause();
 
-            console.log(line)
+			console.log(line)
 
-            // resume the readstream, possibly from a callback
-            s.resume();
-        })
-        .on('error', function(){
-            console.log('Error while reading file.');
-        })
-        .on('end', function(){
-            console.log('Read entire file.')
-        })
-    );
+			// resume the readstream, possibly from a callback
+			s.resume();
+		})
+		.on('error', function(){
+			console.log('Error while reading file.')
+		})
+		.on('end', function(){
+			console.log('Read entire file.')
+		})
+	)
 }
